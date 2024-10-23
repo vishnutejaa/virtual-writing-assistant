af2a4b5c57d5f23360fdeb1b1ddcf20518abb97a
Vishnu Nandam <nandam.vishnutejaa@gmail.com>
2024-10-23 15:23:35 -0700

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WritingAssistant from './WritingAssistant';
+
+function App() {
+  return (
+    <div className="App">
+      <WritingAssistant />
+    </div>
+  );
+}
+
+export default App;
